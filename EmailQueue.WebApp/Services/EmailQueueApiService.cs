1cd3c11170a226785fae00c697bafde2b6ea23e6
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-04-29 16:59:19 -0400

    Accommodate API deployment to a subfolder (#17)

--- EmailQueue.WebApp/Services/EmailQueueApiService.cs
+++ EmailQueue.WebApp/Services/EmailQueueApiService.cs
@@ -2,19 +2,31 @@ using EmailQueue.WebApp.Platform;
 
 namespace EmailQueue.WebApp.Services;
 
-public class EmailQueueApiService(IHttpClientFactory httpClientFactory)
+public class EmailQueueApiService(IHttpClientFactory httpClientFactory, ILogger<EmailQueueApiService> logger)
 {
     public async Task<IEnumerable<EmailTaskViewModel>> GetBatchEmailTasksAsync(string batchId)
     {
         using var client = httpClientFactory.CreateClient(nameof(EmailQueueApiService));
-        client.BaseAddress = new Uri(AppSettings.EmailQueueApi.BaseUrl);
         client.DefaultRequestHeaders.Add("X-API-Key", AppSettings.EmailQueueApi.ApiKey);
+        logger.LogInformation("Getting batch {BatchId}", batchId);
 
-        using var response = await client.GetAsync($"emailTasks/list/{batchId}");
+        using var response = await client.GetAsync(UriCombine(AppSettings.EmailQueueApi.BaseUrl,
+            relativeUri: $"emailTasks/list/{batchId}"));
         response.EnsureSuccessStatusCode();
 
         var emailTasks = await response.Content.ReadFromJsonAsync<List<EmailTaskViewModel>>().ConfigureAwait(false);
         return emailTasks ?? [];
+    }
+
+    private static Uri UriCombine(string baseUrl, string? relativeUri)
+    {
+        var baseUri = new Uri(baseUrl);
+        if (!baseUri.IsAbsoluteUri) throw new ArgumentOutOfRangeException(nameof(baseUrl));
+
+        if (string.IsNullOrEmpty(relativeUri)) return baseUri;
+
+        const char separator = '/';
+        return new Uri(baseUri.ToString().TrimEnd(separator) + separator + relativeUri.TrimStart(separator));
     }
 }
 
